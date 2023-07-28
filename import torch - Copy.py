354ae82021cb57b8f4f1815877a38ddf63f61f12
postdr2050 <project.ai.postdr@gmail.com>
2023-07-28 13:09:57 +0430

    adddd filllllles

---
+++ import torch - Copy.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torchvision.models import alexnet
+import cv2
